fc730e94c35aee3976facae962ec47d1c661a443
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2023-12-06 14:57:20 +0000

    Remove SSM Parameters for podaac bucket

--- workflow-infrastructure/confluence-ssm.tf
+++ workflow-infrastructure/confluence-ssm.tf
@@ -19,17 +19,3 @@ resource "aws_ssm_parameter" "aws_ssm_parameter_edl_token" {
   key_id = aws_kms_key.kms_key_ssm.id
   value  = var.edl_token
 }
-
-resource "aws_ssm_parameter" "aws_ssm_parameter_podaac_key" {
-  name   = "podaac_key"
-  type   = "SecureString"
-  key_id = aws_kms_key.kms_key_ssm.id
-  value  = var.podaac_key
-}
-
-resource "aws_ssm_parameter" "aws_ssm_parameter_podaac_secret" {
-  name   = "podaac_secret"
-  type   = "SecureString"
-  key_id = aws_kms_key.kms_key_ssm.id
-  value  = var.podaac_secret
-}
