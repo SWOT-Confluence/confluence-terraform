c115f824bfd05d3c8c8a30870d77479e57fe1641
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-07-30 21:51:14 +0000

    Create an S3 bucket for config files

--- workflow-infrastructure/confluence-s3.tf
+++ workflow-infrastructure/confluence-s3.tf
@@ -61,3 +61,35 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_jso
     }
   }
 }
+
+# Config S3 Bucket
+resource "aws_s3_bucket" "aws_s3_bucket_config" {
+  bucket        = "${var.prefix}-config"
+  force_destroy = true
+  tags          = { Name = "${var.prefix}-config" }
+}
+
+resource "aws_s3_bucket_public_access_block" "aws_s3_bucket_config_public_block" {
+  bucket                  = aws_s3_bucket.aws_s3_bucket_config.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_config_ownership" {
+  bucket = aws_s3_bucket.aws_s3_bucket_config.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_config_encryption" {
+  bucket = aws_s3_bucket.aws_s3_bucket_config.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = "aws/s3"
+    }
+  }
+}
