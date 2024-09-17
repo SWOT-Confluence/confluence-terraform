f8145b97ad26f780f1394247abd073ce98f962c9
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-09-17 16:12:33 +0000

    Change S3 encryption to type supported by step function result writer

--- workflow-infrastructure/confluence-s3.tf
+++ workflow-infrastructure/confluence-s3.tf
@@ -120,8 +120,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_map
   bucket = aws_s3_bucket.aws_s3_bucket_map.bucket
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = "aws/s3"
+      sse_algorithm     = "AES256"
     }
   }
 }
