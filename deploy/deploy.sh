f9d949e38d0330c3b4171b65b9514b64da5730eb
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-11-14 20:21:41 +0000

    Modify step function for GitHub action workflow

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@
 #
 # Command line arguments:
 # [1] s3_state_bucket: Name of the S3 bucket to store Terraform state in (no need for s3:// prefix)
-# [2] terraform_state_key: Name of Terraform state key (e.g. confluence.tfstate)
+# [2] terraform_state_key: Name of Terraform state key (e.g. confluence.tfstate or confluence-sfn.tfstate)
 # [3] terraform_directory: Which directory to deploy (e.g. workflow-infrastructure)
 # 
 # Example usage: ./deploy.sh "s3-state-bucket-name" "terraform-state-key" "terraform-directory"
