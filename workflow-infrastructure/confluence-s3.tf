be7bcb56c85aa6e226db75243483d22ab51ce2e8
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-12-10 18:18:34 +0000

    Enable AWS managed S3 bucket encryption and bucket key for SOS S3

--- workflow-infrastructure/confluence-s3.tf
+++ workflow-infrastructure/confluence-s3.tf
@@ -24,9 +24,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "aws_s3_bucket_sos
   bucket = aws_s3_bucket.aws_s3_bucket_sos.bucket
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = "aws/s3"
+      sse_algorithm     = "AES256"
     }
+    bucket_key_enabled = true
   }
 }
 
