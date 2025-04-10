ef9eb0ffa60c2fe010e3b95805837ae781bcefc8
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-10 13:22:16 -0400

    Remove provider from Terraform module

--- workflow-infrastructure/main.tf
+++ workflow-infrastructure/main.tf
@@ -1,23 +1,3 @@
-terraform {
-  backend "s3" {
-    encrypt = true
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  default_tags {
-    tags = local.default_tags
-  }
-  region  = var.aws_region
-}
-
 # Data sources
 data "aws_caller_identity" "current" {}
 
