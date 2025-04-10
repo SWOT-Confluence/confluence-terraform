d951632e7742a0ce79da6d87e9ca00b2458f8506
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-10 14:08:54 -0400

    Update to root module execution of child infrastructure module

---
+++ main.tf
@@ -0,0 +1,43 @@
+ terraform {
+  backend "s3" {
+    encrypt = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = local.default_tags
+  }
+  region  = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id = sensitive(data.aws_caller_identity.current.account_id)
+  default_tags = length(var.default_tags) == 0 ? {
+    application : var.app_name,
+    environment : lower(var.environment),
+    version : var.app_version
+  } : var.default_tags
+}
+
+module "confluence-terraform" {
+  source            = "./workflow-infrastructure"
+  app_name          = var.app_name
+  app_version       = var.app_version
+  aws_region        = var.aws_region
+  ec2_key_pair      = var.ec2_key_pair
+  sns_email_reports = var.sns_email_reports
+  environment       = var.environment
+  prefix            = var.prefix
+  vpc_id            = var.vpc_id
+  vpc_sg_id         = var.vpc_sg_id
+  vpc_subnets       = var.vpc_subnets
+}
---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable "app_name" {
+  type        = string
+  description = "Application name"
+  default     = "confluence"
+}
+
+variable "app_version" {
+  type        = string
+  description = "The application version number"
+  default     = "1.0.0"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy to"
+  default     = "us-west-2"
+}
+
+variable "default_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "ec2_key_pair" {
+  type        = string
+  description = "Key pair used to access EFS EC2 instances"
+}
+
+variable "sns_email_reports" {
+  type        = string
+  description = "Email address to SNS topic reports to"
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment in which to deploy to"
+}
+
+variable "permissions_boundary" {
+  type = string
+  description = "Determine if permission boundary needs to be set"
+  default = null
+}
+
+variable "prefix" {
+  type        = string
+  description = "Prefix to add to all AWS resources as a unique identifier"
+}
+
+variable "vpc_id" {
+  type = string
+  description = "Default VPC identifier"
+}
+
+variable "vpc_sg_id" {
+  type = string
+  description = "Default VPC security group"
+}
+
+variable "vpc_subnets" {
+  type = list(string)
+  description = "VPC subnet identifiers"
+}
