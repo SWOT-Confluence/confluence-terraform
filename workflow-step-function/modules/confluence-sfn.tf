9f3fef944a2271ff148ddd3c14904b1c24e4d93e
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-11 15:28:01 -0400

    Update path to Step Function definition

--- workflow-step-function/modules/confluence-sfn.tf
+++ workflow-step-function/modules/confluence-sfn.tf
@@ -2,7 +2,7 @@
 resource "aws_sfn_state_machine" "confluence_state_machine" {
   name       = "${var.prefix}-workflow"
   role_arn   = aws_iam_role.step_function_role.arn
-  definition = templatefile("confluence-sfn-workflow.asl.json", { aws_region = var.aws_region, account_id = local.account_id, prefix = var.prefix })
+  definition = templatefile("${path.module}/confluence-sfn-workflow.asl.json", { aws_region = var.aws_region, account_id = local.account_id, prefix = var.prefix })
   logging_configuration {
     log_destination        = "${aws_cloudwatch_log_group.generate_cw_log_group_sfn.arn}:*"
     include_execution_data = true
