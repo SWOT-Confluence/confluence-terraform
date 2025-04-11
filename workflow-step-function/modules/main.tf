a26ce3fdb98003be552b8af11c1cd5189df31e52
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-11 15:26:29 -0400

    Update to use child module

---
+++ workflow-step-function/modules/main.tf
@@ -0,0 +1,16 @@
+# Data sources
+data "aws_caller_identity" "current" {}
+
+data "aws_s3_bucket" "s3_json" {
+  bucket = "${var.prefix}-json"
+}
+
+# Local variables
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  default_tags = length(var.default_tags) == 0 ? {
+    application : var.app_name,
+    environment : lower(var.environment),
+    version : var.app_version
+  } : var.default_tags
+}
