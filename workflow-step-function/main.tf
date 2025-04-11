d58d4c4fa3c17dfc9feb168e113aac8f31e3f5ea
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-04-11 15:34:45 -0400

    Update state and module directory

--- workflow-step-function/main.tf
+++ workflow-step-function/main.tf
@@ -29,7 +29,7 @@ locals {
 }
 
 module "confluence-terraform" {
-  source            = "./modules"
+  source            = "./modules/sfn"
   app_name          = var.app_name
   app_version       = var.app_version
   aws_region        = var.aws_region
