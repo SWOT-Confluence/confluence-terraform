74236dae91133ad67ec703e3276477ce42f01d0f
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2025-05-21 07:58:12 -0400

    Remove cloudwatch log groups from infrastructure repo (#11)
    * Remove cloudwatch log groups from infrastructure repo
    
    * Add module outputs
    
    * Update id attribute for efs file system resources
    
    ---------
    
    Co-authored-by: Joshua Garde <jgarde@jpl.nasa.gov>

---
+++ workflow-infrastructure/modules/infra/output.tf
@@ -0,0 +1,20 @@
+output "exec_iam_role_arn" {
+  value = aws_iam_role.ecs_exe_task_role.arn
+}
+
+output "batch_job_iam_role_arn" {
+  value = aws_iam_role.batch_job_role.arn
+}
+
+output "efs_file_system_ids" {
+  value = {
+    input = aws_efs_file_system.efs_fs_in.id
+    flpe = aws_efs_file_system.efs_fs_flpe.id
+    moi = aws_efs_file_system.efs_fs_moi.id
+    diagnostics = aws_efs_file_system.efs_fs_diag.id
+    offline = aws_efs_file_system.efs_fs_off.id
+    validation = aws_efs_file_system.efs_fs_val.id
+    output = aws_efs_file_system.efs_fs_out.id
+    logs = aws_efs_file_system.efs_fs_logs.id
+  }
+}
