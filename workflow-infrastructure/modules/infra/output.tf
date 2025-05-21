a72e9229a28d094a9a8627a35bdd3c8c6c29e009
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-05-21 07:54:55 -0400

    Update id attribute for efs file system resources

--- workflow-infrastructure/modules/infra/output.tf
+++ workflow-infrastructure/modules/infra/output.tf
@@ -8,13 +8,13 @@ output "batch_job_iam_role_arn" {
 
 output "efs_file_system_ids" {
   value = {
-    input = aws_efs_file_system.efs_fs_in.file_system_id
-    flpe = aws_efs_file_system.efs_fs_flpe.file_system_id
-    moi = aws_efs_file_system.efs_fs_moi.file_system_id
-    diagnostics = aws_efs_file_system.efs_fs_diag.file_system_id
-    offline = aws_efs_file_system.efs_fs_off.file_system_id
-    validation = aws_efs_file_system.efs_fs_val.file_system_id
-    output = aws_efs_file_system.efs_fs_out.file_system_id
-    logs = aws_efs_file_system.efs_fs_logs.file_system_id
+    input = aws_efs_file_system.efs_fs_in.id
+    flpe = aws_efs_file_system.efs_fs_flpe.id
+    moi = aws_efs_file_system.efs_fs_moi.id
+    diagnostics = aws_efs_file_system.efs_fs_diag.id
+    offline = aws_efs_file_system.efs_fs_off.id
+    validation = aws_efs_file_system.efs_fs_val.id
+    output = aws_efs_file_system.efs_fs_out.id
+    logs = aws_efs_file_system.efs_fs_logs.id
   }
 }
