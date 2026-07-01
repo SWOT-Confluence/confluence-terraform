10cbe0d5e680c74440bc25bbd1ba1003cb6dee06
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2026-07-01 14:23:00 -0700

    Adding ssc workflow

--- workflow-step-function/modules/sfn/confluence-sfn.tf
+++ workflow-step-function/modules/sfn/confluence-sfn.tf
@@ -13,8 +13,29 @@ resource "aws_sfn_state_machine" "confluence_state_machine" {
   }
 }
 
+# Confluence Step Function State Machine
+resource "aws_sfn_state_machine" "confluence_ssc_state_machine" {
+  name       = "${var.prefix}-workflow-ssc"
+  role_arn   = aws_iam_role.step_function_role.arn
+  definition = templatefile("${path.module}/confluence-sfn-workflow-ssc.asl.json", { aws_region = var.aws_region, account_id = local.account_id, prefix = var.prefix })
+  logging_configuration {
+    log_destination        = "${aws_cloudwatch_log_group.confluence_ssc_cw_log_group_sfn.arn}:*"
+    include_execution_data = true
+    level                  = "ALL"
+  }
+  tracing_configuration {
+    enabled = true
+  }
+}
+
 # CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "generate_cw_log_group_sfn" {
   name              = "/aws/states/${var.prefix}-workflow/"
+  retention_in_days = 0
+}
+
+# CloudWatch Log Group
+resource "aws_cloudwatch_log_group" "confluence_ssc_cw_log_group_sfn" {
+  name              = "/aws/states/${var.prefix}-workflow-ssc/"
   retention_in_days = 0
 }
